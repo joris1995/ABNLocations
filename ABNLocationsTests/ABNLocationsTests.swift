1ab1a793275c4c64c530fc41c37945a540cd70cb
Joris Dijkstra <joris.dijkstra@incentro.com>
2024-10-04 23:44:10 +0200

    Initial Commit

---
+++ ABNLocationsTests/ABNLocationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ABNLocationsTests.swift
+//  ABNLocationsTests
+//
+//  Created by Joris Dijkstra on 04/10/2024.
+//
+
+import Testing
+@testable import ABNLocations
+
+struct ABNLocationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
